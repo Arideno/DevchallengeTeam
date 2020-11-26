5674be345cc7b37abec0b457ae0aad1bf80722ca
Andrii Moisol <andrii.moisol@gmail.com>
2020-11-26 16:44:25 +0200

    Add test users

--- server/migrations/000006_create_users_table.up.sql
+++ server/migrations/000006_create_users_table.up.sql
@@ -3,4 +3,7 @@ CREATE TABLE users(
     username varchar not null unique,
     password varchar not null,
     country_id int references countries
-)
+);
+
+INSERT INTO users(username, password, country_id) VALUES ('TestAustralia', '$2a$04$YS1k0R.QaKJbF7U/UJdG/eY0tEm193vneUtVj1oOsg6ljUK5hiNS6', 1);
+INSERT INTO users(username, password, country_id) VALUES ('TestAustria', '$2a$04$YS1k0R.QaKJbF7U/UJdG/eY0tEm193vneUtVj1oOsg6ljUK5hiNS6', 2);
